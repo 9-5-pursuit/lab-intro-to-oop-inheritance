eee9bd520658340dd00e60abdb6bfcaeb0ce25c8
Devin Lewis <devinlewis@pursuit.org>
2023-09-21 12:15:09 -0400

    Added constructor for food and prepare method

--- food.js
+++ food.js
@@ -1,4 +1,13 @@
 // Create class below
-
+class Food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    }
+    prepare() {
+        console.log(`food ${this.name} is being prepared`);
+    }
+}
 // Do not edit below this line
 module.exports = Food;
