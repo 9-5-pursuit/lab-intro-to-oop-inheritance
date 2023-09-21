1211956a9d0f3ae9118fdd8e71e987287823adb6
Devin Lewis <devinlewis@pursuit.org>
2023-09-21 12:56:15 -0400

    Altered Tamagotchi class added timePasses method

--- tamagotchi.js
+++ tamagotchi.js
@@ -64,6 +64,17 @@ class Tamagotchi {
         this.energy += 4;
         this.full -= 3;
     }
+    timePasses() {
+        if (!this.sick) {
+            this.mood -= 2;
+            this.full--;
+            this.energy--;
+        } else {
+            this.mood -= 3;
+            this.full -= 2;
+            this.energy -= 2;
+        }
+    }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
