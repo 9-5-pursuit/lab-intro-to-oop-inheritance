93f47d1a39b7d59cd7342407ce614856ccd54261
davep80 <davidpaq1@gmail.com>
2023-09-21 12:04:26 -0400

    new food

--- food.js
+++ food.js
@@ -1,4 +1,33 @@
 // Create class below
 
+
+class Food {
+    constructor(name, d, b=true) {
+        this.name = name;
+        this.daysToSpoil = d;
+        this.fresh = b;
+    }
+
+    prepare() {
+        console.log(`${this.name} is being prepared`);
+    }
+
+    isFresh() {
+        let mssg = this.daysToSpoil ? `There are ${this.daysToSpoil} days left before
+        ${this.name} spoils.` : `${this.name} has spoiled`
+        console.log(mssg);
+    }
+
+    aDayPasses() {
+        this.daysToSpoil--;
+        this.isFresh();
+    }
+}
+
+let testf = new Food('ice cream', 0)
+testf.prepare();
+testf.isFresh();
+testf.aDayPasses();
+
 // Do not edit below this line
 module.exports = Food;
