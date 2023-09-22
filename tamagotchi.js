321f9d632c42dcc27eefa43690f712245e0a2aa1
Irfan Hanjra <ihanjra@gmail.com>
2023-09-22 02:31:44 -0400

    add play method

--- tamagotchi.js
+++ tamagotchi.js
@@ -39,6 +39,24 @@ class Tamagotchi {
             this.energy -= 1
         }
     }
+
+    play() {
+        if (this.sick === true) {
+            this.mood -= 1;
+            this.energy -= 1;
+        } else if (this.mood > 9) {
+            this.energy -= 2
+            this.full -= 1
+        } else if (this.energy <= 3) {
+            console.log("I am too tired to play")
+            this.energy -= 1
+        }
+        else {
+            this.mood += 2;
+            this.energy -= 1;
+            this.full -= 1;
+        }
+    }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
