203adf192d6b95bf9d11808883bc3f21ec905859
Chantal <chantalgomez@pursuit.org>
2023-09-22 09:48:50 -0400

    All test passed

--- food.js
+++ food.js
@@ -1,129 +1,31 @@
 // Create class below
-// class Food {
-//     constructor( name, daysToSpoil, fresh = true){
-//         this.name = name
-//         this.daysToSpoil = daysToSpoil
-//         this.fresh = fresh
-//     }
+class Food {
+    constructor( name, daysToSpoil, fresh = true){
+        this.name = name
+        this.daysToSpoil = daysToSpoil
+        this.fresh = fresh
+    }
 
-//     prepare(){
-//         console.log(`${this.name} is being prepared`);
-//     }
+    prepare(){
+        console.log(`${this.name} is being prepared`);
+    }
 
-//     isFresh(){
-//         if(this.daysToSpoil  >= 1 ){
-//           console.log(`There are ${this.daysToSpoil} days 
-//           left before ${this.name} spoils.`);
-//         }else{
-//           console.log(`The ${this.name} has spoiled.`);  
-//         }
-//     }
+    isFresh(){
+        if(this.daysToSpoil  >= 1 ){
+          console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
+        }else{
+          console.log(`${this.name} has spoiled.`);  
+        }
+    }
 
-//     aDayPasses(){
-//         this.daysToSpoil--
-//         this.isFresh()
+    aDayPasses(){
+        this.daysToSpoil--
+        this.isFresh()
       
-//     }
-// }
-
-
-class Tamagotchi {    
-    constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false){
-    this.name = name
-    this.energy = energy
-    this.full = full
-    this.mood = mood
-    this.sick = sick
-    this.rehomed = rehomed
-}
-
-
-greet(){
-    console.log(`Hello, I'm ${this.name}!`);
-}
-
-status(){
-    console.log(`My energy is ${this.energy}, I am this full ${this.full}, My mood is ${this.mood}, My sick status is ${this.sick}`);
-}
-
-eat(){
-    this.full++
-    this.full++
-    this.energy--
-
-    if(this.full > 10){
-        this.sick === true
     }
 }
 
-medicate(){
-    if(this.sick === true){
-        this.full === 9
-        this.energy--
-        this.energy--
-        this.energy--
-    }else{
-        console.log(`${this.name} is not sick!`);
-        this.energy--
-    }
-}
 
-play(){
-    if(this.sick === true){
-        console.log(`${this.name} is sick. Medicate them!`);
-        this.mood--
-    }
 
-    if(this.mood > 9){
-        console.log(`${this.name} is happy`);
-        this.energy--
-        this.energy--
-        this.full--
-    }
-
-    if(this.energy <= 3){
-        console.log("I am too tired to play");
-        this.energy--
-    }
-}
-
-sleep(){
-    this.energy++
-    this.energy++
-    this.energy++
-    this.energy++
-  
-    this.full--
-    this.full--
-    this.full--
-}
-
-timePasses(){
-    if(this.sick === false){
-        this.mood--
-        this.mood--
-      
-        this.full--
-        this.energy--
-    }else{
-        this.mood--
-        this.mood--
-        this.mood--
-        this.full--
-        this.full--
-        this.energy--
-        this.energy--
-    }
-  console.log("time")
-}
-
-badGuardian(){
-    if( this.energy <= 0 || this.mood <= 0 || this.full <= 0){
-        console.log(`${this.name} has been rehomed`);
-        this.rehomed === true
-    }
-
-}
-}
 // Do not edit below this line
-module.exports = Tamagotchi;
+module.exports = Food;
