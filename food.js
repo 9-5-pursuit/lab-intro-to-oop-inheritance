3ad98313d994c0bf88a80b3953069da895c38193
YasserS-21 <saadi.yasser04@gmail.com>
2023-10-02 18:00:08 -0400

    add isFresh method Food

--- food.js
+++ food.js
@@ -8,6 +8,15 @@ class Food {
     prepare() {
         console.log(`${this.name} is being prepared`)
     }
+    isFresh(){
+        if (this.daysToSpoil<=0){
+            console.log(`${this.name} has spoiled.`)
+            this.fresh = false
+        } else if (this.fresh){
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        }
+    }
+
 }
 // Do not edit below this line
 module.exports = Food;
