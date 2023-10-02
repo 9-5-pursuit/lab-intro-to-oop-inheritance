2e5277298d10999d3826734e0a0dd05e22eeb21c
YasserS-21 <saadi.yasser04@gmail.com>
2023-10-02 18:17:50 -0400

    add prepare method BadFood

--- tamagotchi.js
+++ tamagotchi.js
@@ -76,6 +76,9 @@ class BadFood extends Food {
         super(name, daysToSpoil, fresh)
         this.weapons= [{'hitpoints':3},{'hitpoints':4},{'hitpoints':5}]
     }
+    prepare(){
+        console.log(`I am ${this.name} and my calories are too high to count!`)
+    }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
