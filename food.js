5ef78fcd562f134e205db72c3454a376c8024562
YasserS-21 <saadi.yasser04@gmail.com>
2023-10-02 18:07:26 -0400

    add aDayPasses method Food

--- food.js
+++ food.js
@@ -16,6 +16,10 @@ class Food {
             console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
         }
     }
+    aDayPasses(){
+        this.daysToSpoil--
+        this.isFresh()
+    }
 
 }
 // Do not edit below this line
