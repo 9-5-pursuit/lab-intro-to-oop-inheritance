faa5b48056f66c8bc2d32ebe1c2f01b8fdd1e137
YasserS-21 <saadi.yasser04@gmail.com>
2023-10-02 17:57:03 -0400

    Food Constructor/Properties

--- food.js
+++ food.js
@@ -1,4 +1,10 @@
 // Create class below
-
+class Food {
+    constructor(name, daysToSpoil, fresh) {
+        this.name = name
+        this.daysToSpoil = daysToSpoil
+        this.fresh = fresh || true
+    }
+}
 // Do not edit below this line
 module.exports = Food;
