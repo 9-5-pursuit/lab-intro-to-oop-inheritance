60f87e14f3337ae3ecc631151e20dd20618a6df1
YasserS-21 <saadi.yasser04@gmail.com>
2023-10-02 17:59:00 -0400

    add prepare method Food

--- food.js
+++ food.js
@@ -5,6 +5,9 @@ class Food {
         this.daysToSpoil = daysToSpoil
         this.fresh = fresh || true
     }
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
 }
 // Do not edit below this line
 module.exports = Food;
