0eaebe7c78420a51dd00334a94ddd892bb6fb14b
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-10-13 02:42:17 -0400

    greet, status, and eat method added

--- tamagotchi.js
+++ tamagotchi.js
@@ -8,6 +8,28 @@ class Tamagotchi {
         this.sick = false;
         this.rehomed = false;
     }
+
+    greet(){
+        console.log(`Hello I'm ${this.name}!`)
+    }
+
+    status(){
+        if (this.sick){
+            console.log(`My mood is: ${this.mood}\n I am this full: ${this.full}\n My energy is: ${this.energy}\n I am not sick`)
+        } else{
+            console.log(
+              `My mood is: ${this.mood}\n I am this full: ${this.full}\n My energy is: ${this.energy}\n I am sick`
+            );
+        }
+    }
+
+    eat(){
+        this.full += 2;
+        this.energy--;
+        if (this.full > 10){
+            this.sick = true
+        }
+    }
 }
 
 // Do not edit below this line
