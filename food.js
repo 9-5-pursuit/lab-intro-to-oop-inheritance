11b585787832630f343f250207815a0cb0e4d719
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-10-13 00:26:40 -0400

    added attributes

--- food.js
+++ food.js
@@ -1,4 +1,12 @@
 // Create class below
+class Food {
+    constructor(name, days) {
+        this.name = name;
+        this.daysToSpoil = days;
+        this.fresh = true;
+    }
+
+}
 
 // Do not edit below this line
 module.exports = Food;
