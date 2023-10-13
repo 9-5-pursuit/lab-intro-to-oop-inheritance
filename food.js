07831caf9704f856bd3452213d04c1e8ff183f82
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-10-13 02:17:59 -0400

    prepare and isFresh methods added

--- food.js
+++ food.js
@@ -6,6 +6,17 @@ class Food {
         this.fresh = true;
     }
 
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+
+    isFresh() {
+        if (this.daysToSpoil > 0) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+    } else{
+        console.log(`${this.name} has spoiled.`)
+    }
+    }
 }
 
 // Do not edit below this line
