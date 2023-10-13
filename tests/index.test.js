c5b980552bcce7703131c0764c17384b24065414
daleskiii <aun2793@gmail.com>
2023-10-13 08:53:25 -0400

    finisishito

--- tests/index.test.js
+++ tests/index.test.js
@@ -80,63 +80,63 @@ describe("Food class", () => {
 });
 
 describe("Tamagotchi Class", () => {
-  test("Can call new on Tamagotchi", () => {
+  test.only("Can call new on Tamagotchi", () => {
     const buzz = new Tamagotchi("Buzz");
     expect(buzz).toBeTruthy();
   });
 
-  test("Tamagotchi has name property that is a string", () => {
+  test.only("Tamagotchi has name property that is a string", () => {
     expect(fizz.name).toBe("Fizz");
     expect(typeof fizz.name).toBe("string");
   });
 
-  test("Tamagotchi has energy property that is a number, default value is 9", () => {
+  test.only("Tamagotchi has energy property that is a number, default value is 9", () => {
     expect(fizz.energy).toBe(9);
     expect(typeof fizz.energy).toBe("number");
   });
 
-  test("Tamagotchi has full property that is a number, default value is 8", () => {
+  test.only("Tamagotchi has full property that is a number, default value is 8", () => {
     expect(fizz.full).toBe(8);
     expect(typeof fizz.full).toBe("number");
   });
 
-  test("Tamagotchi has mood property that a number, default value is 6", () => {
+  test.only("Tamagotchi has mood property that a number, default value is 6", () => {
     expect(fizz.mood).toBe(6);
     expect(typeof fizz.mood).toBe("number");
   });
 
-  test("Tamagotchi has sick property that is default false", () => {
+  test.only("Tamagotchi has sick property that is default false", () => {
     expect(fizz.sick).toBe(false);
     expect(typeof fizz.sick).toBe("boolean");
   });
 
-  test("Tamagotchi has rehomed property that is default false", () => {
+  test.only("Tamagotchi has rehomed property that is default false", () => {
     expect(fizz.rehomed).toBe(false);
     expect(typeof fizz.rehomed).toBe("boolean");
   });
 
-  test("Tamagotchi has greet method", () => {
+  test.only("Tamagotchi has greet method", () => {
     expect(typeof fizz.greet).toBe("function");
     expect(fizz.greet).toBeTruthy();
   });
 
-  test("Tamagotchi has status method", () => {
+  test.only("Tamagotchi has status method", () => {
     expect(typeof fizz.status).toBe("function");
     expect(fizz.status).toBeTruthy();
   });
 
-  test("Tamagotchi has eat method", () => {
+  test.only("Tamagotchi has eat method", () => {
     expect(typeof fizz.eat).toBe("function");
     expect(fizz.eat).toBeTruthy();
   });
 
-  test("eat method increases fullness by 2", () => {
+  test.only("eat method increases fullness by 2", () => {
     fizz.eat();
     expect(fizz.full).toBe(10);
     expect(fizz.sick).toBe(false);
   });
 
-  test("eat method decreases energy by 1", () => {
+  test.only("eat method decreases energy by 1", () => {
     fizz.full = 6;
     fizz.energy = 4;
     fizz.eat();
@@ -144,7 +144,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.sick).toBe(false);
   });
 
-  test("if eat method makes fullness more than 10, tamagotchi becomes sick", () => {
+  test.only("if eat method makes fullness more than 10, tamagotchi becomes sick", () => {
     fizz.full = 10;
     fizz.energy = 4;
     fizz.eat();
@@ -152,12 +152,12 @@ describe("Tamagotchi Class", () => {
     expect(fizz.sick).toBe(true);
   });
 
-  test("Tamagotchi has medicate method", () => {
+  test.only("Tamagotchi has medicate method", () => {
     expect(typeof fizz.medicate).toBe("function");
     expect(fizz.medicate).toBeTruthy();
   });
 
-  test("Tamagotchi medicate heals sick tamagotchi", () => {
+  test.only("Tamagotchi medicate heals sick tamagotchi", () => {
     fizz.full = 12;
     fizz.energy = 4;
     fizz.sick = true;
@@ -165,7 +165,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.sick).toBe(false);
   });
 
-  test("Tamagotchi medicate for sick tamagotchi sets full to 9 and decreases energy by 3", () => {
+  test.only("Tamagotchi medicate for sick tamagotchi sets full to 9 and decreases energy by 3", () => {
     fizz.sick = true;
     fizz.full = 12;
     fizz.energy = 4;
@@ -174,19 +174,19 @@ describe("Tamagotchi Class", () => {
     expect(fizz.energy).toBe(1);
   });
 
-  test("Tamagotchi medicate for not sick tamagotchi reduces energy by 1", () => {
+  test.only("Tamagotchi medicate for not sick tamagotchi reduces energy by 1", () => {
     fizz.sick = false;
     fizz.energy = 4;
     fizz.medicate();
     expect(fizz.energy).toBe(3);
   });
 
-  test("Tamagotchi has play method", () => {
+  test.only("Tamagotchi has play method", () => {
     expect(typeof fizz.play).toBe("function");
     expect(fizz.play).toBeTruthy();
   });
 
-  test("Tamagotchi play increases mood by 2, reduces energy & full by 1", () => {
+  test.only("Tamagotchi play increases mood by 2, reduces energy & full by 1", () => {
     fizz.mood = 5;
     fizz.energy = 6;
     fizz.full = 8;
@@ -196,7 +196,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.full).toBe(7);
   });
 
-  test("Tamagotchi won't play if it is sick", () => {
+  test.only("Tamagotchi won't play if it is sick", () => {
     fizz.mood = 10;
     fizz.energy = 6;
     fizz.full = 8;
@@ -207,7 +207,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.full).toBe(8);
   });
 
-  test("Tamagotchi won't play if mood is above 9", () => {
+  test.only("Tamagotchi won't play if mood is above 9", () => {
     fizz.mood = 10;
     fizz.energy = 6;
     fizz.full = 8;
@@ -218,7 +218,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.full).toBe(7);
   });
 
-  test("Tamagotchi won't play if energy is less than or equal to 3", () => {
+  test.only("Tamagotchi won't play if energy is less than or equal to 3", () => {
     fizz.mood = 4;
     fizz.energy = 3;
     fizz.full = 3;
@@ -228,12 +228,12 @@ describe("Tamagotchi Class", () => {
     expect(fizz.full).toBe(3);
   });
 
-  test("Tamagotchi has sleep method", () => {
+  test.only("Tamagotchi has sleep method", () => {
     expect(typeof fizz.sleep).toBe("function");
     expect(fizz.sleep).toBeTruthy();
   });
 
-  test("Sleep method increases energy by 4 and decreases full by 3", () => {
+  test.only("Sleep method increases energy by 4 and decreases full by 3", () => {
     fizz.mood = 4;
     fizz.energy = 1;
     fizz.full = 4;
@@ -243,12 +243,12 @@ describe("Tamagotchi Class", () => {
     expect(fizz.full).toBe(1);
   });
 
-  test("Tamagotchi has timePasses method", () => {
+  test.only("Tamagotchi has timePasses method", () => {
     expect(typeof fizz.timePasses).toBe("function");
     expect(fizz.timePasses).toBeTruthy();
   });
 
-  test("If tamagotchi is not sick, timePasses, mood decreases by 2, fullness and energy decrease by 1", () => {
+  test.only("If tamagotchi is not sick, timePasses, mood decreases by 2, fullness and energy decrease by 1", () => {
     fizz.mood = 9;
     fizz.energy = 9;
     fizz.full = 9;
@@ -258,7 +258,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.full).toBe(8);
   });
 
-  test("If tamagotchi is sick, timePasses, mood decreases by 3, fullness and energy decrease by 2", () => {
+  test.only("If tamagotchi is sick, timePasses, mood decreases by 3, fullness and energy decrease by 2", () => {
     fizz.sick = true;
     fizz.mood = 9;
     fizz.energy = 9;
@@ -269,12 +269,12 @@ describe("Tamagotchi Class", () => {
     expect(fizz.full).toBe(7);
   });
 
-  test("Tamagotchi has badGuardian method", () => {
+  test.only("Tamagotchi has badGuardian method", () => {
     expect(typeof fizz.badGuardian).toBe("function");
     expect(fizz.badGuardian).toBeTruthy();
   });
 
-  test("If energy is less than or equal to 0, rehomed is true", () => {
+  test.only("If energy is less than or equal to 0, rehomed is true", () => {
     fizz.energy = 0;
     fizz.mood = 9;
     fizz.full = 9;
@@ -282,7 +282,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.rehomed).toBe(true);
   });
 
-  test("If mood is less than or equal to 0, rehomed is true", () => {
+  test.only("If mood is less than or equal to 0, rehomed is true", () => {
     fizz.mood = 0;
     fizz.energy = 9;
     fizz.full = 9;
@@ -290,7 +290,7 @@ describe("Tamagotchi Class", () => {
     expect(fizz.rehomed).toBe(true);
   });
 
-  test("If full is less than or equal to 0, rehomed is true", () => {
+  test.only("If full is less than or equal to 0, rehomed is true", () => {
     fizz.mood = 9;
     fizz.energy = 9;
     fizz.full = 0;
