df1e4dc804be39f216f98ff1ea7d8b7aded1a618
Hillary Brito <hillarybrito@Hillarys-MacBook-Air.local>
2023-10-17 01:57:01 -0400

    created greet method

--- tamagotchi.js
+++ tamagotchi.js
@@ -8,6 +8,9 @@ class Tamagotchi {
         this.sick = sick;
         this.rehomed = rehomed;
     }
+    greet() {
+        console.log(`Hello, I'm ${this.name}!`)
+    }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
