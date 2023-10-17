047e4657a794b90c0b7989b58fa2942a6772a2b3
Hillary Brito <hillarybrito@Hillarys-MacBook-Air.local>
2023-10-17 01:31:39 -0400

    updated if else statement for isFresh method

--- food.js
+++ food.js
@@ -9,13 +9,17 @@ class Food {
     console.log(`${this.name} is being prepared`);
   }
   isFresh() {
-    if (this.daysToSpoil > 0) {
-      console.log(
-        `There are ${this.daysToSpoil} days left before ${this.name} spoils`
-      );
+    if (this.daysToSpoil <= 0) {
+     console.log(`${this.name} has spoiled`);
     } else {
-        console.log(`${this.name} has spoiled`)
+        
+         console.log(
+           `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+         );
     }
+  }
+  aDayPasses() {
+    this.daysToSpoil--;
   }
 }
 // Do not edit below this line
