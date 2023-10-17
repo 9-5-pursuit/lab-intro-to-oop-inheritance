fe32bf70cb53cac9807fe426b08c6ef2ea1c486f
Hillary Brito <hillarybrito@Hillarys-MacBook-Air.local>
2023-10-17 02:11:27 -0400

    created status method and message

--- tamagotchi.js
+++ tamagotchi.js
@@ -11,6 +11,12 @@ class Tamagotchi {
     greet() {
         console.log(`Hello, I'm ${this.name}!`)
     }
+    status() {
+        console.log(`My mood is: ${this.mood}
+        I am this full: ${this.full}
+        My energy is: ${this.energy}
+        I am not sick`)
+    }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
