78097c3b102627cc0eb762f1c0c08c7ae4e15a43
ShakaPursuit <shakalivngstone@pursuit.org>
2023-12-10 12:14:04 -0500

    added second edge case to test my new class

--- food.js
+++ food.js
@@ -4,7 +4,7 @@
 // here I created my food class
 class Food{
 
-    constructor(name,daysToSpoil,fresh){//adding constructor and attributes
+    constructor(name,daysToSpoil,fresh=Boolean){//adding constructor and attributes
             this.name=name
             this.daysToSpoil=daysToSpoil
             this.fresh=fresh
@@ -17,6 +17,7 @@ class Food{
 
             console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils`)
         }else if(this.daysToSpoil<=0){
+            this.fresh=false
             console.log(`${this.name} had spoiled.`)
 
         }
@@ -39,16 +40,28 @@ class Food{
 
 
 }
-//test 1
+//My test 1
 
 const  createFoodItem=(name,daysToSpoil)=>{//here i created a function called create food item
     const newFood= new Food(name,daysToSpoil,true)//im declaring newFood to equal the creation of a new Food class, passing params name and days to spoil/ so arguements can be passed in when the function is called/invoked
                                                 
     newFood.isFresh()//checking to see if the created new food is fresh
+    newFood.prepare()// the new item is brand new assuming it will always be fresh then we can go ahead and prepare it
     return newFood // returning the whole process
 
 
 }
 createFoodItem("Pizza",5)
+
+
+//my test 2
+
+const twentyFourHours=(name,daysToSpoil)=>{
+    const newFood= new Food(name,daysToSpoil,true)
+    newFood.aDayPasses()//24 hours has passed less take a day off this food
+
+}
+twentyFourHours('milk',2)//if it works it should take a day off this item
+twentyFourHours('oranges',1)//this should take a day off making it spoiled and retruning the spoiled message from isFresh()
 // Do not edit below this line
 module.exports = Food;
